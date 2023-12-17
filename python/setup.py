2abe1c7a03b0a777cb450ec45f192877a1aa26b0
El nino <el-nino202020@protonmail.com>
2023-12-17 19:49:54 +0800

    add classifiers to setup.py

--- python/setup.py
+++ python/setup.py
@@ -8,5 +8,16 @@ setup(
     url="https://github.com/el-nino2020/script-house",
     author="El nino",
     author_email="el-nino202020@protonmail.com",
-    python_requires=">=3.11"
+    python_requires=">=3.11",
+    classifiers=[
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Other Audience",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3"
+    ]
 )
