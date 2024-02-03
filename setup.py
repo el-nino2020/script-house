5f534545819b938ae35c5cf4928f66dbd8eefa84
El nino <el-nino202020@protonmail.com>
2024-02-03 20:04:02 +0800

    0.1.0 版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name="script_house",
-    version="0.0.2",
+    version="0.1.0",
     description="a python script house containing handy functions for daily use",
     packages=find_packages(),
     url="https://github.com/el-nino2020/script-house",
