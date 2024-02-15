753efbae8ea7a6eef5da2594dd242006cc095009
El nino <el-nino202020@protonmail.com>
2024-02-15 17:36:28 +0800

    0.1.1 版本: 用户自动安装本模块的依赖

--- setup.py
+++ setup.py
@@ -3,15 +3,20 @@ from setuptools import find_packages, setup
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+    requirements = [r.strip() for r in requirements]
+
 setup(
     name="script_house",
-    version="0.1.0",
+    version="0.1.1",
     description="a python script house containing handy functions for daily use",
     packages=find_packages(),
     url="https://github.com/el-nino2020/script-house",
     author="El nino",
     author_email="el-nino202020@protonmail.com",
     python_requires=">=3.11",
+    install_requires=requirements,
     classifiers=[
         "Operating System :: Microsoft :: Windows :: Windows 10",
         "License :: OSI Approved :: Apache Software License",
